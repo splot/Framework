dbf74a1696bccdc016a885ea774b627cba05a8e5
Michał Dudek <michal@michaldudek.pl>
2015-06-12 02:03:05 +0200

    EventErrorHandler will only send the handling response if running in web mode.

--- src/ErrorHandlers/EventErrorHandler.php
+++ src/ErrorHandlers/EventErrorHandler.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Event error handler.
- * 
+ *
  * @package SplotFramework
  * @subpackage ErrorHandlers
  * @author Michał Dudek <michal@michaldudek.pl>
- * 
+ *
  * @copyright Copyright (c) 2014, Michał Dudek
  * @license MIT
  */
@@ -13,39 +13,55 @@ namespace Splot\Framework\ErrorHandlers;
 
 use Whoops\Handler\Handler;
 
+use Splot\DependencyInjection\ContainerInterface;
 use Splot\EventManager\EventManager;
 
 use Splot\Framework\Events\ExceptionDidOccur;
+use Splot\Framework\Framework;
 
 class EventErrorHandler extends Handler
 {
 
     /**
      * Event manager.
-     * 
+     *
      * @var EventManager
      */
     protected $eventManager;
 
     /**
+     * Container.
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
      * Constructor.
      *
+     * @param WhoopsRun $whoops Whoops.
      * @param EventManager $eventManager Splot Event Manager.
+     * @param ContainerInterface $container Container.
      */
-    public function __construct(EventManager $eventManager) {
+    public function __construct(EventManager $eventManager, ContainerInterface $container)
+    {
         $this->eventManager = $eventManager;
+        $this->container = $container;
     }
 
     /**
      * Handle the error by triggering Splot\Framework\Events\ExceptionDidOccur event.
      */
-    public function handle() {
+    public function handle()
+    {
         $event = new ExceptionDidOccur($this->getException());
 
         // trigger the event
         $this->eventManager->trigger($event);
 
-        if (($response = $event->getResponse()) && $event->isHandled()) {
+        // only send the response in actual web mode
+        $mode = $this->container->getParameter('mode');
+        if ($mode == Framework::MODE_WEB && $event->isHandled() && ($response = $event->getResponse())) {
             $response->send();
         }
 
@@ -59,5 +75,4 @@ class EventErrorHandler extends Handler
 
         return Handler::DONE;
     }
-
 }
